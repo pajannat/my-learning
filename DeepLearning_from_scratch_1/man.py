f5e8260bb6f81e230085d663335d1fc2c2d32893
pajannat <61099858+pajannat@users.noreply.github.com>
2022-08-14 13:12:53 +0900

    Add files via upload

---
+++ DeepLearning_from_scratch_1/man.py
@@ -0,0 +1,10 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initilized!")
+        
+    def hello(self):
+        print("Hello " + self.name + "!")
+    
+    def goodby(self):
+        print("Good-bye " + self.name + "!")
